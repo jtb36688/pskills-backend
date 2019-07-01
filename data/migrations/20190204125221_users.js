7a1cb68a2eff4c89d899ec593e927fe53f902a16
Jacob Bryan <jtb36688@gmail.com>
2019-07-01 18:34:28 -0400

    Fixed multiple database errors involving seeds and migrations.

--- data/migrations/20190204125221_users.js
+++ data/migrations/20190204125221_users.js
@@ -1,12 +1,11 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('prisons', tbl => {
+    return knex.schema.createTable('users', tbl => {
       tbl.increments();
-      tbl.string('name', 255).notNullable().unique() ;
-      tbl.string('location', 255).notNullable();
-      tbl.string('phoneNumber', 255);
+      tbl.string('username', 255).notNullable().unique() ;
+      tbl.string('password', 255).notNullable();
     });
   };
   
   exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('prisons');
+    return knex.schema.dropTableIfExists('users');
   };
--- data/migrations/20190205084638_changePrisonersTable.js
+++
@@ -1,9 +0,0 @@
-exports.up = function(knex, Promise) {
-    return knex.schema.alterTable('prisoners', tbl => {
-        tbl.string('skills', 255);
-    });
-  };
-  
-  exports.down = function(knex, Promise) {
-      return knex.schema.dropColumn('skills');
-  };
